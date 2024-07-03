737ffbcd94ad4de317b4f8b35a8b12fc55660708
SankalpKadam <sankalptp07@gmail.com>
2024-07-03 18:15:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json())
 // app.get('/', (req, res) => {
 //     res.send(receipts)
 // })
-app.use(router)
+app.use('/receipts',router)
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
 })
