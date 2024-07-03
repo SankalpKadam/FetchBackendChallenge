587a9d6350320ae7bb61f6be1eb298aeb49c590a
SankalpKadam <sankalptp07@gmail.com>
2024-07-03 16:11:45 -0500

    basic routes setup completed

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
-
+import { receipts } from "./examples/examples.js";
+import router from "./routes/routes.js";
 const app = express();
 const port = 3000;
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send("Server running")
-})
 
+// app.get('/', (req, res) => {
+//     res.send(receipts)
+// })
+app.use(router)
 app.listen(port, ()=>{
     console.log(`Server is listening on port ${port}`);
 })
--- routes/routes.js
+++ routes/routes.js
@@ -1,3 +1,15 @@
 import express from 'express'
 
 const router = express.Router()
+
+// post route for processing the receipt
+router.post('/process',(req, res)=>{
+    res.send('processing receipts')
+})
+
+// get route to the points for the receipts
+router.get('/:id/points', (req, res) => {
+    res.send("32")
+})
+
+export default router;
